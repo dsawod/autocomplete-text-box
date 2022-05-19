bb8b421903a1a50f1e4350e0b91d6e3fc1804ebc
Devendra Sawod <dsawod420@unm.edu>
2022-05-19 15:34:06 -0600

    final commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import MajorUSCities from './MajorUSCities';
+import countries from './countries';
 import AutoCompleteText from './AutoCompleteText';
 
 function App() {
   return (
     <div className="App">
-      <AutoCompleteText />
+      <div className="App-Component">
+        Major US Cities
+        <AutoCompleteText items={MajorUSCities} />
+        <br />
+        Countries
+        <AutoCompleteText items={countries} />
+      </div>
     </div>
   );
 }
