ce6130d9b6e26175a4024ab2da6f6f130b27e0c9
Devendra Sawod <dsawod420@unm.edu>
2022-05-19 14:46:41 -0600

    render list of items that matches text entered

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AutoCompleteText from './AutoCompleteText';
+
+function App() {
+  return (
+    <div className="App">
+      <AutoCompleteText />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/AutoCompleteText.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+export default class AutoCompleteText extends React.Component {
+    constructor(props) {
+        super(props);
+        this.items = [
+            'Deven',
+            'Dre',
+            'Todd',
+            'Mari',
+            'Bethenny',
+            'Sam',
+            'Alex',
+        ];
+        this.state = {
+            suggestions: [],
+        };
+    }
+
+    onTextChanged = (e) => {
+        const value = e.target.value;
+        let suggestions = [];
+        if (value.length > 0) {
+            const regex = new RegExp(`^${value}`, 'i');
+            suggestions = this.items.sort().filter(v => regex.test(v));
+        }
+        this.setState(() => ({ suggestions }));
+    }
+
+    renderSuggestions() {
+        const { suggestions } = this.state;
+        if (suggestions.length === 0) {
+            return null;
+        }
+        return (
+            <ul>
+                {suggestions.map((item) => <li>{item}</li>)}
+            </ul>
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <input onChange={this.onTextChanged} type="text" />
+                {this.renderSuggestions()}
+            </div>
+        )
+    }
+}
